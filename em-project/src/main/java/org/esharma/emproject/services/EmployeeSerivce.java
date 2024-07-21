6a3ea0157c2159883b30b710861b462355d24759
Shivam Sharma <157497911+ExponentSharma@users.noreply.github.com>
2024-07-21 11:59:13 +0530

    Spring Boot - Backend code committing

---
+++ em-project/src/main/java/org/esharma/emproject/services/EmployeeSerivce.java
@@ -0,0 +1,12 @@
+package org.codingwallah.emproject;
+
+import java.util.List;
+
+public interface EmployeeSerivce {
+    String createEmployee(Employee employee);
+    List<Employee> readEmployees();
+    boolean deleteEmployee(Long id);
+    String updateEmployee(Long id, Employee employee);
+    Employee readEmployee(Long id);
+
+}
